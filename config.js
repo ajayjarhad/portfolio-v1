bef97e89bf25a3cff2f91069f2020b9205289458
Ajay Jarhad <ajayjarhad1@gmail.com>
2020-08-27 04:08:13 +0530

    Styled and revamped

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Spectral', // <title>
-  manifestName: 'Spectral',
-  manifestShortName: 'Landing', // max 12 characters
+  siteTitle: 'Ajay Jarhad | Portfolio', // <title>
+  manifestName: 'Portfolio ',
+  manifestShortName: 'v1', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Ajay Jarhad',
   subHeading: 'Full time Web Developer. Part time Open source contributor  ',
   // social
   socialLinks: [
@@ -16,19 +16,19 @@ module.exports = {
       style: 'brands',
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
+      url: 'https://github.com/ajayjarhad',
     },
-    {
-      style: 'brands',
-      icon: 'fa-twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
-    },
+    // {
+    //   style: 'brands',
+    //   icon: 'fa-twitter',
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/onlyanubhav',
+    // },
     {
       style: 'solid',
       icon: 'fa-envelope',
       name: 'Email',
-      url: 'mailto:test@example.com',
+      url: 'mailto:ajayjarhad1@gmail.com',
     },
   ],
 };
