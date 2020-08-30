8e03540b2fa8c7a1d075699c8c4fdb8705315022
Ajay Jarhad <ajayjarhad1@gmail.com>
2020-08-30 23:33:29 +0530

    added personal projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ import Scroll from '../components/Scroll';
 import pic1 from '../assets/images/pic01.jpg';
 import pic2 from '../assets/images/pic02.jpg';
 import pic3 from '../assets/images/pic03.jpg';
+import pic4 from '../assets/images/pic04.jpg';
+import pic5 from '../assets/images/pic05.jpg';
 import config from '../../config';
 import WorkTimeline from '../components/WorkTimeline';
 import CustomizedTimeline from '../components/CustomizedTimeline';
@@ -89,20 +91,43 @@ const IndexPage = () => (
     </section>
 
     <section id="two" className="wrapper alt style2">
+      {/* <h2
+        style={{
+          color: 'white',
+          display: 'flex',
+          justifyContent: 'center',
+
+          borderBottom: '1px solid white',
+        }}
+      >
+        {' '}
+        Some of my apps and websites
+      </h2> */}
+
       <section className="spotlight">
         <div className="image">
           <img src={pic1} alt="" />
         </div>
         <div className="content">
-          <h2>
-            Magna primis lobortis
-            <br />
-            sed ullamcorper
-          </h2>
+          <h2>Personal portfolio website</h2>
           <p>
-            Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
-            imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
+            This website is designed using React.js and Gatsby.js with
+            MaterialUI
           </p>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              height: '3em',
+            }}
+          >
+            <a
+              href="https://github.com/ajayjarhad/portfolio-v1/"
+              className="button primary"
+            >
+              Source
+            </a>
+          </div>
         </div>
       </section>
       <section className="spotlight">
@@ -110,15 +135,34 @@ const IndexPage = () => (
           <img src={pic2} alt="" />
         </div>
         <div className="content">
-          <h2>
-            Tortor dolore feugiat
-            <br />
-            elementum magna
-          </h2>
+          <h2>Face detection AI full stack website</h2>
           <p>
-            Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
-            imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
+            The app is designed using React.js with Redux as a frontend
+            framework with Node.js and Express.js as backend. It uses PostgreSQL
+            to manage users.
           </p>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              height: '2.7rem',
+            }}
+          >
+            <a
+              href="https://face-recongnition-ai.herokuapp.com/"
+              className="button primary"
+              style={{ marginTop: '-1em' }}
+            >
+              Demo
+            </a>
+            <a
+              href="https://github.com/ajayjarhad/face_recognition_ai/"
+              className="button primary"
+              style={{ marginTop: '-1em' }}
+            >
+              Source
+            </a>
+          </div>
         </div>
       </section>
       <section className="spotlight">
@@ -126,15 +170,72 @@ const IndexPage = () => (
           <img src={pic3} alt="" />
         </div>
         <div className="content">
-          <h2>
-            Augue eleifend aliquet
-            <br />
-            sed condimentum
-          </h2>
+          <h2>Global Covid-19 tracker</h2>
           <p>
-            Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
-            imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
+            The web app uses React.js with Hooks and MaterialUI and Chart.js
           </p>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              height: '2.7rem',
+            }}
+          >
+            <a
+              href="https://covid-19-tracker-reactjs.herokuapp.com/"
+              className="button primary"
+              style={{ marginTop: '-1em' }}
+            >
+              Demo
+            </a>
+            <a
+              href="https://github.com/ajayjarhad/covid-19-tracker/"
+              className="button primary"
+              style={{ marginTop: '-1em' }}
+            >
+              Source
+            </a>
+          </div>
+        </div>
+      </section>
+      <section className="spotlight">
+        <div className="image">
+          <img src={pic4} alt="" />
+        </div>
+        <div className="content">
+          <h2>Techni Kalash's Website</h2>
+          <p>The website uses HTML,CSS,JS and Bootstrap 4</p>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              height: '3em',
+            }}
+          >
+            <a href="http://technikalash.com/" className="button primary">
+              Demo
+            </a>
+          </div>
+        </div>
+      </section>
+      <section className="spotlight">
+        <div className="image">
+          <img src={pic5} alt="" />
+        </div>
+        <div className="content">
+          <h2>Trading In Zones's Website</h2>
+          <p>The website uses Wordpress</p>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              height: '3em',
+            }}
+          >
+            <a href="http://tradinginzones.com/" className="button primary">
+              Demo
+            </a>
+          </div>
         </div>
       </section>
     </section>
